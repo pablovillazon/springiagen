e449331c1aa79ed9a510c6cc5f7e5c1ca60164a6
pablovillazon <p.villazon@gmail.com>
2025-02-14 21:24:35 -0400

    Template inicial del proyecto de SpringBoot

---
+++ src/main/java/com/dccg2/iagenerated/IageneratedApplication.java
@@ -0,0 +1,14 @@
+package com.dccg2.iagenerated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IageneratedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IageneratedApplication.class, args);
+                System.out.println("Hello mundo");
+	}
+
+}
